bd60f890350202bf38fe43cd505f5126cdb59fca
guru-sulakhe <sulakheguruprasad@gmail.com>
2025-04-11 19:32:19 +0530

    adding account_ids for pipelines deployments

--- var/pipelineGlobals.groovy
+++ var/pipelineGlobals.groovy
@@ -7,3 +7,20 @@ static final String nexusURL(){
 static final String account_id(){
     return "637423540068"
 }
+
+def getAccountID(String environment){
+    switch(environment) { 
+        case 'dev': 
+            return "315069654700"
+        case 'qa':
+            return "315069654700"
+        case 'uat':
+            return "315069654700"
+        case 'pre-prod':
+            return "315069654700"
+        case 'prod':
+            return "315069654700"
+        default:
+            return "nothing"
+    } 
+}
